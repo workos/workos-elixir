d6f73d372bbd9b414c0b2d245af50d1205b6423f
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2021-04-20 14:24:39 -0400

    0.1.2 (#2)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule WorkOS.MixProject do
     [
       name: "WorkOS SDK for Elixir",
       app: :workos,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       description: description(),
