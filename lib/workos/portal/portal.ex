f6d72b6f1494e8a0d461e8074de752e3ff6d2328
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-12-19 17:58:16 -0300

    Remove organization functions from Portal module

--- lib/workos/portal/portal.ex
+++ lib/workos/portal/portal.ex
@@ -9,44 +9,6 @@ defmodule WorkOS.Portal do
   """
 
   @doc """
-  Create an organization
-
-  ### Parameters
-  - params (map)
-    - domains (list of strings) List of domains that belong to the organization
-    - name (string) A unique, descriptive name for the organization
-
-  ### Example
-  WorkOS.Portal.create_organization(%{
-    domains: ["workos.com"],
-    name: "WorkOS"
-  })
-  """
-  def create_organization(params, opts \\ [])
-
-  def create_organization(params, opts)
-      when is_map_key(params, :domains) and is_map_key(params, :name) do
-    query = process_params(params, [:domains, :name])
-    post("/organizations", query, opts)
-  end
-
-  def create_organization(_params, _opts),
-    do: raise(ArgumentError, message: "need both domains and name in params")
-
-  @doc """
-  Delete an organization
-
-  ### Parameters
-   - organization_id (string) the id of the organization to delete
-
-  ### Example
-  WorkOS.Portal.delete_organization("organization_12345")
-  """
-  def delete_organization(organization, opts \\ []) do
-    delete("/organizations/#{organization}", %{}, opts)
-  end
-
-  @doc """
   Generate a link to grant access to an organization's Admin Portal
 
   ### Parameters
@@ -74,27 +36,4 @@ defmodule WorkOS.Portal do
 
   def generate_link(_params, _opts),
     do: raise(ArgumentError, message: "need both intent and organization in params")
-
-  @doc """
-  Retrieve a list of organizations that have connections configured
-  within your WorkOS dashboard.
-
-  ### Parameters
-  - params (map)
-    - domains (array of strings) Filter organizations to only return those
-     that are associated with the provided domains.
-    - before (string) A pagination argument used to request
-     organizations before the provided Organization ID.
-    - after (string) A pagination argument used to request
-     organizations after the provided Organization ID.
-    - limit (integer) A pagination argument used to limit the number
-      of listed Organizations that are returned.
-
-  ### Example
-  WorkOS.Portal.list_organizations()
-  """
-  def list_organizations(params \\ %{}, opts \\ []) do
-    query = process_params(params, [:domains, :limit, :before, :after])
-    get("/organizations", query, opts)
-  end
 end
