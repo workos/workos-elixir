02bf3bf5625e5fb3037adb981767e6cb9efb25bc
jacobiajohnson <jacobia@workos.com>
2023-03-08 11:10:45 -0500

    Bump minor version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule WorkOS.MixProject do
     [
       name: "WorkOS SDK for Elixir",
       app: :workos,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       description: description(),
