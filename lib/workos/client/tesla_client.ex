2052843769d5c118199b95a99bcdf40481e9f2d5
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-10-21 13:37:22 -0300

    Add `TeslaClient` implementation

--- lib/workos/client/tesla_client.ex
+++ lib/workos/client/tesla_client.ex
@@ -3,4 +3,28 @@ defmodule WorkOs.Client.TeslaClient do
   Tesla client for WorkOs. This is the default HTTP client used.
   """
   @behaviour WorkOs.Client
+
+  @doc """
+  Sends a request to a WorkOs API endpoint, given list of request opts.
+  """
+  @spec request(Resend.Client.t(), Keyword.t()) ::
+          {:ok, %{body: map(), status: pos_integer()}} | {:error, any()}
+  def request(client, opts) do
+    opts = Keyword.take(opts, [:method, :url, :query, :headers, :body, :opts])
+    Tesla.request(new(client), opts)
+  end
+
+  @doc """
+  Returns a new `Tesla.Client`, configured for calling the WorkOs API.
+  """
+  @spec new(Resend.Client.t()) :: Tesla.Client.t()
+  def new(client) do
+    Tesla.client([
+      Tesla.Middleware.Logger,
+      {Tesla.Middleware.BaseUrl, client.base_url},
+      Tesla.Middleware.PathParams,
+      Tesla.Middleware.JSON,
+      {Tesla.Middleware.Headers, [{"Authorization", "Bearer #{client.api_key}"}]}
+    ])
+  end
 end
