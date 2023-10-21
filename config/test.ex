c122e7c800d2cfeb09f055a330b3162f32c04f94
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-10-21 15:16:06 -0300

    Fix test config

--- config/test.ex
+++ config/test.ex
@@ -1,8 +1,9 @@
 import Config
 
-if workos_api_key = System.get_env("WORKOS_API_KEY") do
-  config :workos, WorkOS.Client, api_key: workos_api_key
+if workos_api_key =
+     System.get_env("WORKOS_API_KEY") and workos_client_id = System.get_env("WORKOS_CLIENT_ID") do
+  config :workos, WorkOS.Client, api_key: workos_api_key, client_id: workos_client_id
 else
   config :tesla, adapter: Tesla.Mock
-  config :workos, WorkOS.Client, api_key: "re_123456789"
+  config :workos, WorkOS.Client, api_key: "test_123", client_id: "test_123"
 end
