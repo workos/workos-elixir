c337813bc3b8df3a4fc03024090afac017470039
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-10-23 20:53:24 -0300

    Extract env variables to separate variables

--- config/test.ex
+++ config/test.ex
@@ -1,7 +1,9 @@
 import Config
 
-if workos_api_key =
-     System.get_env("WORKOS_API_KEY") and workos_client_id = System.get_env("WORKOS_CLIENT_ID") do
+workos_api_key = System.get_env("WORKOS_API_KEY")
+workos_client_id = System.get_env("WORKOS_CLIENT_ID")
+
+if workos_api_key and workos_client_id do
   config :workos, WorkOS.Client, api_key: workos_api_key, client_id: workos_client_id
 else
   config :tesla, adapter: Tesla.Mock
