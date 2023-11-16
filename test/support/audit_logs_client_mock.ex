cc02e2b61e11a6819cc3b7b4921fec7c8e2ba014
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-11-16 19:04:19 -0300

    Add tests for `create_export`

--- test/support/audit_logs_client_mock.ex
+++ test/support/audit_logs_client_mock.ex
@@ -2,4 +2,34 @@ defmodule WorkOS.AuditLogs.ClientMock do
   @moduledoc false
 
   use ExUnit.Case
+
+  def create_export(context, opts \\ []) do
+    Tesla.Mock.mock(fn request ->
+      %{api_key: api_key} = context
+
+      assert request.method == :post
+      assert request.url == "#{WorkOS.base_url()}/audit_logs/exports"
+
+      assert Enum.find(request.headers, &(elem(&1, 0) == "Authorization")) ==
+               {"Authorization", "Bearer #{api_key}"}
+
+      body = Jason.decode!(request.body)
+
+      for {field, value} <- Keyword.get(opts, :assert_fields, []) do
+        assert body[to_string(field)] == value
+      end
+
+      success_body = %{
+        "object" => "audit_log_export",
+        "id" => "audit_log_export_1234",
+        "state" => "pending",
+        "url" => nil,
+        "created_at" => "2022-02-15T15:26:53.274Z",
+        "updated_at" => "2022-02-15T15:26:53.274Z"
+      }
+
+      {status, body} = Keyword.get(opts, :respond_with, {200, success_body})
+      %Tesla.Env{status: status, body: body}
+    end)
+  end
 end
--- test/workos/audit_logs_test.exs
+++ test/workos/audit_logs_test.exs
@@ -4,4 +4,23 @@ defmodule WorkOS.AuditLogsTest do
   alias WorkOS.AuditLogs.ClientMock
 
   setup :setup_env
+
+  describe "create_export" do
+    test "with valid payload, creates an audit log export", context do
+      opts = [
+        organization_id: "org_01EHWNCE74X7JSDV0X3SZ3KJNY",
+        range_start: "2022-06-22T15:04:19.704Z",
+        range_end: "2022-08-22T15:04:19.704Z",
+        actions: ["user.signed_in"],
+        actors: ["Jon Smith"],
+        targets: ["team"]
+      ]
+
+      context
+      |> ClientMock.create_export(assert_fields: opts)
+
+      assert {:ok, %WorkOS.AuditLogs.Export{}} =
+               opts |> Map.new() |> WorkOS.AuditLogs.create_export()
+    end
+  end
 end
