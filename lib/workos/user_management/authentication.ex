8cb40a94ca83dceb491f4d208d6c889e0c8fad9a
William Huba <william@jumpwire.io>
2024-07-22 10:11:43 -0400

    Add OAuth token to WorkOS.UserManagement.Authentication struct
    Logging in a user with a code returns an OAuth access token and
    refresh token in the API response. Currently these tokens are
    discarded when casting the Authentication struct - which also means
    the claims from the JWT are lost.

--- lib/workos/user_management/authentication.ex
+++ lib/workos/user_management/authentication.ex
@@ -9,22 +9,35 @@ defmodule WorkOS.UserManagement.Authentication do
 
   @type t() :: %__MODULE__{
           user: User.t(),
-          organization_id: String.t() | nil
+          organization_id: String.t() | nil,
+          access_token: String.t() | nil,
+          refresh_token: String.t() | nil,
+          impersonator: User.t() | nil,
+          authentication_method: String.t()
         }
 
   @enforce_keys [
-    :user
+    :user,
+    :authentication_method
   ]
   defstruct [
     :user,
-    :organization_id
+    :organization_id,
+    :access_token,
+    :refresh_token,
+    :impersonator,
+    :authentication_method
   ]
 
   @impl true
   def cast(map) do
     %__MODULE__{
       user: map["user"],
-      organization_id: map["organization_id"]
+      organization_id: map["organization_id"],
+      access_token: map["access_token"],
+      refresh_token: map["refresh_token"],
+      impersonator: map["impersonator"],
+      authentication_method: map["authentication_method"]
     }
   end
 end
