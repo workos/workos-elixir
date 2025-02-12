5002c97de63c21cfd27cc3879cffb23ec165f6f2
Adrien Anselme <adanselm@gmail.com>
2025-02-12 14:52:50 +0100

    upgrade hackney version

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule WorkOS.MixProject do
   defp deps do
     [
       {:tesla, "~> 1.4"},
-      {:hackney, "~> 1.19.0"},
+      {:hackney, "~> 1.20.1"},
       {:jason, ">= 1.0.0"},
       {:plug_crypto, "~> 1.0"},
       {:ex_doc, "~> 0.23", only: :dev, runtime: false},
